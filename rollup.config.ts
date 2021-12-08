2894ee7774a0ebb4b2fa09591ac0fff7eb816449
Hiroki Omote <h.omote@scheeme.com>
2021-12-08 18:53:34 +0900

    build: add release command

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,7 +1,17 @@
+import path from 'path'
+import alias from '@rollup/plugin-alias'
+
 export default {
   input: './plugins/components.ts',
   output: {
     file: 'dist/index.js',
     format: 'es'
-  }
+  },
+  plugins: [
+    alias({
+      entries: {
+        '@': path.resolve(__dirname, './')
+      }
+    })
+  ]
 }
